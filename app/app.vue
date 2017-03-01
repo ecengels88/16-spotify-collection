69c5b87c19d0baa5ee1e6a6d9c40423fe4bf1c38
Evan Engels <evan.engels@gmail.com>
2017-03-01 11:41:12 -0600

    created song item and app folder, input html

---
+++ app/app.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="app">
+    <input type="text" name="" value="" class="search">
+    <div class="track-list">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import SongItem from '.song-item';
+
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
---
+++ app/song-item.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div class="track-list__card">
+    <div class="album-cover">
+      <img src="#" alt="" class="album-cover__pic">
+    </div>
+    <div class="track-info">
+      <h1 class="track-info__artist"></h1>
+      <p class="track-info__song"></p>
+    </div>
+    <div class="spotify-image">
+      <img src="#" alt="" class="spotify-image__pic">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
