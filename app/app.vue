8aa99e117a703b133cda836d003d747f5d677a40
Evan Engels <evan.engels@gmail.com>
2017-03-06 10:41:42 -0600

    fixed the search bar

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,18 @@
 <template lang="html">
-  <div class="panel">
-    <!-- <form action=""> -->
-      <input class="panel-heading" v-on:submit="search">
-    <!-- </form> -->
-      <div class="results">
-<song-item  v-for="current in tracks" v-bind:track="current"></song-item>
+  <div class="section">
+    <div class="container">
+      <div class="panel">
+        <!-- <form action=""> -->
+          <p class="panel-heading">
+
+            <input class="input" placeholder="Search For Your Band" v-on:keydown.enter="search(searchTerm)" v-model="searchTerm">
+          </p>
+        <!-- </form> -->
+          <div class="results">
+      <song-item  v-for="current in tracks" v-bind:track="current"></song-item>
+          </div>
       </div>
+    </div>
   </div>
 </template>
 
@@ -19,18 +26,19 @@ export default {
   },
 
   created() {
-    this.search('black keys');
+    this.search('Hugh Laurie');
   },
 
   data() {
     return {
       tracks: [],
+      searchTerm: '',
     };
   },
 
   methods: {
     search(bandName) {
-      fetch('https://api.spotify.com/v1/search?query=`${bandName}`&type=track&offset=0&limit=20')
+      fetch(`https://api.spotify.com/v1/search?query=${bandName}&type=track&offset=0&limit=20`)
       .then(r => r.json())
       .then((curr) => {
         this.tracks = curr.tracks.items;
